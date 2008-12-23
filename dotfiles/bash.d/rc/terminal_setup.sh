2fc86c4206f775b5ff0d445fede90189f7a095b0
Damien Pollet <damien.pollet@inria.fr>
2008-12-23 03:37:37 +0100

    Cleanup & putting stuff in the right place

--- dotfiles/bash.d/rc/terminal_setup.sh
+++ dotfiles/bash.d/rc/terminal_setup.sh
@@ -1,9 +1,14 @@
 export LC_CTYPE=en_US.UTF-8
 
-export LSCOLORS='ExGxFxDaCxDaDaAcAcAeEx'
+# Share command history across terminals, lookup if path cache outdated
+shopt -s histappend checkhash
+
+export PROMPT_COMMAND='history -a'
 
 nameMachineDirPrompt='\[\e[01;32m\]\u\[\e[00;32m\]@\[\e[01;32m\]\h \[\e[01;33m\]\W \[\e[00;00m\]'
 hourDirPrompt='\[\e[00;32m\]\A \[\e[01;33m\]\W \[\e[00m\]'
 dirPrompt='\[\e[00;01;32;07m\] \[\e[27m\] \W \[\e[00m\]'
 export PS1=$dirPrompt
 export PS2='\[\e[00;32m\]> \[\e[00;00m\]'
+
+export LSCOLORS='ExGxFxDaCxDaDaAcAcAeEx'
