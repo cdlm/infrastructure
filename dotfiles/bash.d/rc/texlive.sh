a7e2041b9f97a6128a9492df690f66d21f064ec9
Damien Pollet <damien.pollet@inria.fr>
2008-12-24 01:12:21 +0100

    update architecture in texlive

--- dotfiles/bash.d/rc/texlive.sh
+++ dotfiles/bash.d/rc/texlive.sh
@@ -1,5 +1,5 @@
 # standalone texlive
 texlivePrefix=/usr/local/texlive/${texliveVersion:=2008}
-export PATH=$texlivePrefix/bin/`uname -p`-darwin:$PATH
+export PATH=$texlivePrefix/bin/universal-darwin:$PATH
 export MANPATH=$texlivePrefix/texmf/doc/man:`manpath -q`
 export INFOPATH=$texlivePrefix/texmf/doc/info:$INFOPATH
