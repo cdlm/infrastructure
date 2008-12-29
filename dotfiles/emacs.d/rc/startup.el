e27b61ff70466ef3a691ae466679326b454f436b
Damien Pollet <damien.pollet@inria.fr>
2008-12-30 00:22:55 +0100

    Initial emacs support

---
+++ dotfiles/emacs.d/rc/startup.el
@@ -0,0 +1,2 @@
+(setq inhibit-startup-message t) 
+; (setq initial-scratch-message nil)
---
+++ dotfiles/emacs.d/setup.el
@@ -0,0 +1,13 @@
+;;;
+;;; personal emacs setup, to be loaded from ~/.emacs
+;;;
+
+;; lisp extensions
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+;; load configuration modules
+(require 'elisp-load-dir)
+(elisp-load-dir "~/.emacs.d/rc")
+
+;; misc one-liners
+(require 'tabbar)
