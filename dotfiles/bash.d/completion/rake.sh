ae73a99a3fc401d912e23fd839e1626422db5c5e
Damien Pollet <damien.pollet@inria.fr>
2009-02-10 12:52:14 +0100

    Fix stale completion script

--- dotfiles/bash.d/completion/rake.sh
+++ dotfiles/bash.d/completion/rake.sh
@@ -1,2 +1,9 @@
-complete -C ~/Tools/scripts/rake-complete.rb -o default rake
-complete -C ~/Tools/scripts/rake-complete.rb -o default rote
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+_rakecomplete() {
+  COMPREPLY=($(compgen -W "`rake -s -T | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
+  return 0
+}
+
+complete -o default -o nospace -F _rakecomplete rake
+# complete -C ~/Tools/scripts/rake-complete.rb -o default rote
