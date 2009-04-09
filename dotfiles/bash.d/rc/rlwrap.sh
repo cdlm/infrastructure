2d4b70b34585d93b84e48b45927e06ac5799c513
Damien Pollet <damien.pollet@inria.fr>
2009-04-09 09:09:59 +0200

    Stupid quotes

--- dotfiles/bash.d/rc/rlwrap.sh
+++ dotfiles/bash.d/rc/rlwrap.sh
@@ -2,6 +2,6 @@ if [ -n `which rlwrap` ]; then
   export RLWRAP_HOME=~/.rlwrap.d
   mkdir -p $RLWRAP_HOME
   
-  alias ocaml='rlwrap -p'1;34' -r ocaml'
+  alias ocaml='rlwrap -p"1;34" -r ocaml'
 
 fi
