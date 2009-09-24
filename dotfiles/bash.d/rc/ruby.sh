d3fac5b06c4ff8ad01a815754b653da42398d894
Damien Pollet <damien.pollet@inria.fr>
2009-09-24 21:29:51 +0200

    Homedir rubygems

---
+++ dotfiles/bash.d/rc/ruby.sh
@@ -0,0 +1,6 @@
+# That one seems useless since gem does the right thing when run without sudo.
+# Also, the default layout separates 1.8 and 1.9 while setting GEM_HOME doesn't.
+#export GEM_HOME=$HOME/.gem
+
+export RUBYOPT=rubygems
+export PATH=$HOME/.gem/ruby/1.8/bin:$PATH
