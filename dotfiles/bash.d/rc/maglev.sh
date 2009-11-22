3de64ceca1240cd03cb4a7713aed88ed7ca8d420
Damien Pollet <damien.pollet@inria.fr>
2009-11-22 14:21:28 +0100

    GemStone and MagLev

---
+++ dotfiles/bash.d/rc/maglev.sh
@@ -0,0 +1,17 @@
+# GemStone and Maglev
+gemstoneRoot=/opt/gemstone
+
+### installation procedure:
+# mkdir $gemstoneRoot && cd $gemstoneRoot
+# git clone git://github.com/MagLev/maglev.git maglev.gitclone
+# cd maglev.gitclone
+# ./install.sh     # this will pull GemStone too, use update.sh afterwards
+# ln -s ...        # to avoid versions in the PATH
+#
+# For the frontend GemTools, get http://seaside.gemstone.com/squeak/GemTools-MagLev.zip
+
+if [ -d $gemstoneRoot ]; then
+  export GEMSTONE_HOME=$gemstoneRoot/current
+  export MAGLEV_HOME=$gemstoneRoot/maglev
+  export PATH=$MAGLEV_HOME/bin:$GEMSTONE_HOME/bin:$PATH
+fi
