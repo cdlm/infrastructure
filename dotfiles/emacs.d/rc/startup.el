c32d32edf3850bb5891a04b51f0839100c1f3b6f
Damien Pollet <damien.pollet@inria.fr>
2010-01-15 23:41:22 +0100

    Fix emacs scratch buffer message

--- dotfiles/emacs.d/rc/startup.el
+++ dotfiles/emacs.d/rc/startup.el
@@ -1,4 +1,5 @@
 (setq inhibit-startup-message t) 
-(setq initial-scratch-message ";;;   S C R A T C H   B U F F E R   ;;;")
+(setq initial-scratch-message ";;;   S C R A T C H   B U F F E R   ;;;
+")
 
 (menu-bar-mode nil)
