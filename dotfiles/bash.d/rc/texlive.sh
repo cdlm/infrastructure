9d351d921c8824d74cd3ab5ec6f16daf4efe0d2e
Damien Pollet <damien.pollet@inria.fr>
2010-01-16 20:22:11 +0100

    Default for TeXlive is 2009

--- dotfiles/bash.d/rc/texlive.sh
+++ dotfiles/bash.d/rc/texlive.sh
@@ -2,7 +2,7 @@
 if [ -f ~/.texliveVersion ]; then
     texliveVersion=$(cat ~/.texliveVersion)
 fi
-texlivePrefix=/usr/local/texlive/${texliveVersion:=2008}
+texlivePrefix=/usr/local/texlive/${texliveVersion:=2009}
 export PATH=$texlivePrefix/bin/universal-darwin:$PATH
 export MANPATH=$texlivePrefix/texmf/doc/man:`manpath -q`
 export INFOPATH=$texlivePrefix/texmf/doc/info:$INFOPATH
