5ed1efff653c42969aa098815a4f19422cde0a12
Damien Pollet <damien.pollet@inria.fr>
2010-01-19 02:52:00 +0100

    Show git status in command prompt

--- dotfiles/bash.d/rc/terminal_setup.sh
+++ dotfiles/bash.d/rc/terminal_setup.sh
@@ -2,17 +2,25 @@ export LC_CTYPE=en_US.UTF-8
 
 # Share command history across terminals, lookup if path cache outdated
 shopt -s histappend checkhash
-
 export PROMPT_COMMAND='history -a'
+
+# Show git branch & status in prompt
+function _parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+}
+function _parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(_parse_git_dirty)]/"
+}
+
+# nameMachineDirPrompt='\[\e[01;32m\]\u\[\e[00;32m\]@\[\e[01;32m\]\h \[\e[01;33m\]\W'
+# hourDirPrompt='\[\e[00;32m\]\A \[\e[01;33m\]\W'
+dirPrompt='\[\e[0;92;7m\] \[\e[27;1m\] \W\[\e[0;32m\]$(_parse_git_branch)'
 
 screenTitleEscape='\[\033k\033\\\]'
 promptEnd=' \[\e[0m\]'
 if [ $TERM != ${TERM/screen} ]; then
 	promptEnd=${screenTitleEscape}'\[\e[0;32m\] \$'${promptEnd}
 fi
-# nameMachineDirPrompt='\[\e[01;32m\]\u\[\e[00;32m\]@\[\e[01;32m\]\h \[\e[01;33m\]\W'
-# hourDirPrompt='\[\e[00;32m\]\A \[\e[01;33m\]\W'
-dirPrompt='\[\e[0;92;7m\] \[\e[27;1m\] \W'
 
 export PS1=${dirPrompt}${promptEnd}
 export PS2='\[\e[00;32m\]> \[\e[00;00m\]'
