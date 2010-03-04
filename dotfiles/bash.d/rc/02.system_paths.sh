907124949c79030d8ba78c94dafeffa5270ce90b
Damien Pollet <damien.pollet@gmail.com>
2010-03-04 14:15:55 +0100

    Add developer binaries in $PATH (clang etc)

--- dotfiles/bash.d/rc/02.system_paths.sh
+++ dotfiles/bash.d/rc/02.system_paths.sh
@@ -1,2 +1,3 @@
-export PATH=$HOME/bin:/usr/local/bin:/Developer/Tools:$PATH
+export PATH=$HOME/bin:/usr/local/bin:/Developer/Tools:/Developer/usr/bin:$PATH
 # export PATH=$PATH:/usr/X11R6/bin
+export MANPATH=/Developer/usr/share/man:`manpath -q`
