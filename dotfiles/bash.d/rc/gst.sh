909f6b0f874b0c02c814bb1749442e4dfadca963
Damien Pollet <damien.pollet@gmail.com>
2010-11-15 23:41:13 +0100

    Use gst from homebrew instead

--- dotfiles/bash.d/rc/gst.sh
+++ dotfiles/bash.d/rc/gst.sh
@@ -1,8 +1,3 @@
-gstDir=/opt/gnu-smalltalk
-if [ -x $gstDir/bin/gst ]; then
-	export PATH=$gstDir/bin:$PATH
-
-  if [ -n `which rlwrap` ]; then
-    alias gst='rlwrap -p"1;34" -r gst'
-  fi
+if which -s gst rlwrap; then
+   alias gst='rlwrap -p"1;34" -r gst'
 fi
