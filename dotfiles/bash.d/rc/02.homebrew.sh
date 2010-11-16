8c9821d0cbe6d94d2b01390308b4528407825091
Damien Pollet <damien.pollet@gmail.com>
2010-11-16 22:18:24 +0100

    Add homebrew's sbin to PATH

--- dotfiles/bash.d/rc/02.homebrew.sh
+++ dotfiles/bash.d/rc/02.homebrew.sh
@@ -1,4 +1,4 @@
 if [ -d /opt/homebrew ]; then
-   export PATH=/opt/homebrew/bin:$PATH
+   export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
    source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
 fi
