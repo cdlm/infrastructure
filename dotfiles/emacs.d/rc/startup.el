316b0308b0dded984bbb6d2b24f129a2aeefe6ba
Damien Pollet <damien.pollet@gmail.com>
2010-12-16 03:08:42 +0100

    Save font settings here as well as in custom

--- dotfiles/emacs.d/rc/startup.el
+++ dotfiles/emacs.d/rc/startup.el
@@ -6,8 +6,12 @@
 
 (if window-system
     (progn
+        ; make forward-delete work
         (global-set-key [kp-delete] 'delete-char)
         (global-set-key [M-kp-delete] 'kill-word)
+        ; make command the META key, and option access accents properly
+        (setq ns-alternate-modifier (quote none))
+        (setq ns-command-modifier (quote meta))
         (menu-bar-mode 1)
         (tool-bar-mode 0))
    (menu-bar-mode 0))
