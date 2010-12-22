7e1becb72152f361abd2bce345e956d8a307ea32
Damien Pollet <damien.pollet@gmail.com>
2010-12-22 21:32:48 +0100

    Use bash completion from homebrew

--- dotfiles/bash.d/rc/02.homebrew.sh
+++ dotfiles/bash.d/rc/02.homebrew.sh
@@ -1,4 +1,8 @@
 if [ -d /opt/homebrew ]; then
    export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
    source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
+   for f in `brew --prefix`/etc/bash_completion.d/*; do
+      source $f
+   done
+   unset f
 fi
