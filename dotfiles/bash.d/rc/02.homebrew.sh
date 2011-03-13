65ee133b193bfb709be2dabb649063f360b25e21
Damien Pollet <damien.pollet@gmail.com>
2011-03-13 23:38:28 +0100

    brew completion should be already symlinked in the right place

--- dotfiles/bash.d/rc/02.homebrew.sh
+++ dotfiles/bash.d/rc/02.homebrew.sh
@@ -3,7 +3,7 @@ bp='/opt/homebrew' #`brew --prefix`
 if [ -d $bp ]; then
    pathmunge PATH $bp/bin
    pathmunge PATH $bp/sbin
-   for f in $bp/etc/bash_completion $bp/Library/Contributions/brew_bash_completion.sh; do
+   for f in $bp/etc/bash_completion; do 
       [ -f $f ] && source $f
    done
 fi
