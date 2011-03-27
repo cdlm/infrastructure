1032a28f6e48ea843da0bf084d8d1bd69227ef40
Damien Pollet <damien.pollet@gmail.com>
2011-03-27 22:58:03 +0200

    Somewhat fix emacs movement keys

--- dotfiles/emacs.d/rc/editing.el
+++ dotfiles/emacs.d/rc/editing.el
@@ -5,3 +5,19 @@
   (newline-and-indent))
 
 (global-set-key (kbd "C-<return>") 'open-next-line)
+
+;; movement keys
+(global-set-key (kbd "M-z") 'undo)
+
+(global-set-key (kbd "C-<kp-delete>") 'kill-word)
+
+(global-set-key (kbd "A-<left>") 'backward-word)
+(global-set-key (kbd "A-<right>") 'forward-word)
+
+(global-set-key (kbd "M-<left>") 'beginning-of-visual-line)
+(global-set-key (kbd "M-<right>") 'end-of-visual-line)
+(global-set-key (kbd "<home>") 'beginning-of-line)
+(global-set-key (kbd "<end>") 'end-of-line)
+
+(global-set-key (kbd "M-<up>") 'beginning-of-buffer)
+(global-set-key (kbd "M-<down>") 'end-of-buffer)
