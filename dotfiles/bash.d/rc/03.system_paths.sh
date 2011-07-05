12eaebae435a277946670bf15dd907e73033ff64
Damien Pollet <damien.pollet@gmail.com>
2011-07-05 14:11:02 +0200

    /usr/local/bin in PATH redundant with /etc/paths

--- dotfiles/bash.d/rc/03.system_paths.sh
+++ dotfiles/bash.d/rc/03.system_paths.sh
@@ -1,6 +1,6 @@
 pathmunge PATH /Developer/Tools
 pathmunge PATH /Developer/usr/bin
-pathmunge PATH /usr/local/bin
+# pathmunge PATH /usr/local/bin
 pathmunge PATH $HOME/bin
 # export PATH=$PATH:/usr/X11R6/bin
 # export MANPATH=/Developer/usr/share/man:`manpath -q`
