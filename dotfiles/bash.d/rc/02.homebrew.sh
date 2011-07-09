226692d1b758e659c13fd01d0cf63db76ccbd4da
Damien Pollet <damien.pollet@gmail.com>
2011-07-09 21:41:31 +0200

    Path for homebrew python executables

--- dotfiles/bash.d/rc/02.homebrew.sh
+++ dotfiles/bash.d/rc/02.homebrew.sh
@@ -1,8 +1,9 @@
 bp='/opt/homebrew' #`brew --prefix`
 
 if [ -d $bp ]; then
-   pathmunge PATH $bp/bin
    pathmunge PATH $bp/sbin
+   pathmunge PATH $bp/bin
+   pathmunge PATH $bp/share/python
    for f in $bp/etc/bash_completion; do 
       [ -f $f ] && source $f
    done
