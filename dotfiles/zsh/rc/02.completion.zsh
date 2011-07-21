e11b39d5048a8fe97633f58f3f5338e017a15abf
Damien Pollet <damien.pollet@gmail.com>
2011-07-21 03:00:55 +0200

    Zsh completion settings & appearance

--- dotfiles/zsh/rc/02.completion.zsh
+++ dotfiles/zsh/rc/02.completion.zsh
@@ -1,7 +1,18 @@
 require compinit
-
-zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+require solarized
 
 zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion:*' completer _complete _prefix
+
+zstyle ':completion:*' list-colors "$LS_COLORS"
+
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=38;12;6'
+
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:(descriptions|messages)' format "%{$BG[base02]$FG[base1]%}‣ %d %{$FX[reset]%}"
+zstyle ':completion:*:warnings' format "%{$BG[base02]$FG[base2]%}‣ no match: %d %{$FX[reset]%}"
+
--- dotfiles/zsh/rc/terminal.zsh
+++ dotfiles/zsh/rc/terminal.zsh
@@ -2,6 +2,7 @@ export HISTSIZE=5000
 export HISTFILE=~/.zsh_history
 export SAVEHIST=$HISTSIZE
 
+# history options
 setopt share_history
 setopt extended_history
 setopt hist_ignore_all_dups
@@ -9,7 +10,19 @@ setopt hist_expire_dups_first
 setopt hist_ignore_space
 setopt append_history
 setopt inc_append_history
+setopt hist_reduce_blanks
 
+# completion setup
+setopt auto_list
+setopt auto_menu
+setopt list_packed
+setopt bang_hist
+setopt no_hash_list_all
+
+# ls colors, assuming gnu coreutils from homebrew
+if command which -s gdircolors; then
+    [[ -f ~/.dircolors ]] && eval `gdircolors ~/.dircolors`
+fi
 export LSCOLORS='exgxdadacxfafaAcAcAeex'
 export GREP_OPTIONS='--color=auto'
 
