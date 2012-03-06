fcfaafde76c8358bcb0790a2918156dc116c7adb
Damien Pollet <damien.pollet@gmail.com>
2012-03-06 15:54:46 +0100

    Zsh: More precise completion of TeX files

--- dotfiles/zsh/rc/01.completion.zsh
+++ dotfiles/zsh/rc/01.completion.zsh
@@ -9,12 +9,19 @@ zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion:*' completer _complete _prefix _files
 
 zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=38;12;6'
+zstyle ':completion:*:*:*:*:processes' command \
+    "ps -u `whoami` -o pid,user,comm -w -w"
+zstyle ':completion:*:*:kill:*:processes' list-colors \
+    '=(#b) #([0-9]#) ([0-9a-z-]#)*=38;12;6'
 
 zstyle ':completion:*:matches' group 'yes'
 zstyle ':completion:*:options' description 'yes'
 zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:(descriptions|messages)' format "%{$BG[base02]$FG[base1]%}‣ %d %{$FX[reset]%}"
-zstyle ':completion:*:warnings' format "%{$BG[base02]$FG[base2]%}‣ no match: %d %{$FX[reset]%}"
+zstyle ':completion:*:(descriptions|messages)' format \
+    "%{$BG[base02]$FG[base1]%}‣ %d %{$FX[reset]%}"
+zstyle ':completion:*:warnings' format \
+    "%{$BG[base02]$FG[base2]%}‣ no match: %d %{$FX[reset]%}"
 
+zstyle ':completion:*:*:(latexmk|(|la|pdf|pdfla|xe|xela|lua|luala)tex):*' file-patterns \
+    '*.(tex|latex|ltx|dtx|ins):tex-files:TeX\ source' \
+    '%p:all-files:all\ other\ files'
