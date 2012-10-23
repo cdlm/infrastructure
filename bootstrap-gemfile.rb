65c14fd95550c90e71cee5fb126268a23c953669
Damien Pollet <damien.pollet@gmail.com>
2012-10-23 22:22:29 +0200

    List of basic gems to install

---
+++ bootstrap-gemfile.rb
@@ -0,0 +1,21 @@
+# default gems to install in a scratch ruby install
+# install with:
+#   gem install bundler
+#   bundle install --gemfile=bootstrap-gemfile.rb
+source :rubygems
+
+gem 'bundler' # this one needs to be done by hand…
+
+# general utilities
+gem 'jump'
+
+# ruby development
+gem 'pry'
+gem 'yard'
+
+# writing
+gem 'trex'
+gem 'kramdown'
+
+# vim: ft=ruby
+
