b8460eba75ae6b6811fc910dad6dcc0cc583a190
Damien Pollet <damien.pollet@gmail.com>
2013-12-13 16:16:35 +0100

    Zsh: python testing aliases

--- dotfiles/zsh/rc/python.zsh
+++ dotfiles/zsh/rc/python.zsh
@@ -1,3 +1,6 @@
 # setup for globally installed python software, e.g. with brew-pip
 export PYTHONPATH=$bp/lib/python2.7/site-packages
 export PATH=$PATH:$bp/share/python
+
+alias pytest='nosetests -v --with-doctest'
+alias doctest='python -m doctest'
