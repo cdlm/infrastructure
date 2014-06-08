06a63f5208ce9fa25581d1b129b02fb0e1a24de5
Damien Pollet <damien.pollet@gmail.com>
2014-06-08 17:13:06 +0200

    Zsh: remove unnecessary python dirs from $PATH

--- dotfiles/zsh/rc/python.zsh
+++ dotfiles/zsh/rc/python.zsh
@@ -1,5 +1,4 @@
 # setup for globally installed python software, e.g. with brew-pip
-export PATH=$PATH:$bp/share/python
 
 alias pytest='nosetests -v --with-doctest'
 alias doctest='python -m doctest'
