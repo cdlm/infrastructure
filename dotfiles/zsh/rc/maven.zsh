7607ab192156cb909e49bbfe1c13d263c3ba0d31
Damien Pollet <damien.pollet@gmail.com>
2015-02-21 13:38:27 +0100

    Zsh: headless java is more annoying than useful

--- dotfiles/zsh/rc/maven.zsh
+++ dotfiles/zsh/rc/maven.zsh
@@ -2,5 +2,5 @@ if has_command mvn; then
     export M3_HOME=$bp/opt/maven/libexec
     # block java launcher/booters from stealing focus
     # seems more general than MAVEN_OPTS
-    export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
+    # export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
 fi
