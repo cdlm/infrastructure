3b74aebd833b393e6e284c7c414ed23a794c6706
Damien Pollet <damien.pollet@gmail.com>
2016-03-01 00:18:31 +0100

    Zsh: source iTerm2 integration script if present

--- dotfiles/zsh/rc/terminal.zsh
+++ dotfiles/zsh/rc/terminal.zsh
@@ -76,6 +76,8 @@ autoload -U zcalc
 unalias run-help
 autoload run-help
 
+[ -f ~/.iterm2_shell_integration.zsh ] && source ~/.iterm2_shell_integration.zsh
+
 # per-directory environment setup
 if has_command direnv; then
     export DIRENV_RUBY=/usr/bin/ruby
