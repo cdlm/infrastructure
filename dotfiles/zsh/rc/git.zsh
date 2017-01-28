160d813a90057a238030e63afe20f0cc9819132f
Damien Pollet <damien.pollet@gmail.com>
2017-01-28 15:45:12 +0100

    Zsh: do not setup GIT_SUBREPO_ROOT

--- dotfiles/zsh/rc/git.zsh
+++ dotfiles/zsh/rc/git.zsh
@@ -1,3 +1,3 @@
 # homebrew's recipe doesn't include the zsh completions, but that could be added
 # to fpath here as well...
-has_command git-subrepo && export GIT_SUBREPO_ROOT=$bp/opt/git-subrepo
+# has_command git-subrepo && export GIT_SUBREPO_ROOT=$bp/opt/git-subrepo
